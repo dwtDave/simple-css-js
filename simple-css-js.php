9ef468d3ce981aeebae0b87a9403c59c37c608d2
Dave <david@dwt.ie>
2023-10-20 13:44:47 +0100

    Initial commit

---
+++ simple-css-js.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Plugin Name: DBYW
+Plugin URI: 
+Description: A WordPress plugin to manage JavaScript and CSS files for your wordpress site
+Version: 1.0
+Author: Dave
+License:GNU General Public License (GPL) v3
+*/
+
+
+//prevent direct access to this plugin
+defined('ABSPATH') or die('No script kiddies please!');
+
+
+function dbyw_plugin_enqueue_assets() {
+    // Enqueue CSS
+    wp_enqueue_style('custom-css-style', plugin_dir_url(__FILE__) . 'style.css');
+
+    // Enqueue JavaScript
+    wp_enqueue_script('custom-js-script', plugin_dir_url(__FILE__) . 'script.js', [], '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'dbyw_plugin_enqueue_assets');
+
+
